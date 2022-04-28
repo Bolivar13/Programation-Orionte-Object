1ef67d0b611dde31d132199881e49adde111e3e1
Bolivar 13 <97998346+Bolivar13@users.noreply.github.com>
2022-04-28 00:21:28 +0000

    Add files via upload

---
+++ l'Epression(re)/credi.py
@@ -0,0 +1,40 @@
+import re
+
+s = input("Saisir les dimensions du boitier :")
+
+
+
+def calculer_volume ():
+    re.findall ("\d+)(m/cm/mm)")
+    if s :
+        return True
+    else: 
+        return False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
